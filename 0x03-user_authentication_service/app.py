2b84759cb34098c4af6ad6d8b11eebffec4fcf97
amxsupport <itmasterdesigne@gmail.com>
2024-05-27 22:57:56 +0000

    update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,4 +18,19 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"}), 200
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def user() -> str:
+    """ POST /users
+    Return:
+      - message
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
 
