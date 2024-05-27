0aa3cebe2e7da8f2852a174ec1174a3a821d029e
amxsupport <itmasterdesigne@gmail.com>
2024-05-27 22:56:25 +0000

    create app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Flask class
+"""
+
+from auth import Auth
+from flask import Flask, jsonify, request, abort, redirect
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """ GET /
+    Return:
+      - welcome
+    """
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+
