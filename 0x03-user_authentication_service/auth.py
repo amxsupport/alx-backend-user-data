71cd46b715c8026bd5fbab512f693f9cd5d55f6a
amxsupport <itmasterdesigne@gmail.com>
2024-05-27 22:48:55 +0000

    update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,4 +24,12 @@ def _generate_uuid() -> str:
     return str(uuid4())
 
 
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
 
