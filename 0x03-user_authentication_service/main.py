8b65e1b80e3cac5299de14626fdc3f0d6a0d002c
amxsupport <itmasterdesigne@gmail.com>
2024-05-28 18:03:55 +0000

    create main.py

---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+End-to-end integration test
+"""
+
+
+def register_user(email: str, password: str) -> None:
+    """
+    register_user.
+    """
+    assert True
+    return
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    log_in_wrong_password.
+    """
+    assert True
+    return
+
+
+def log_in(email: str, password: str) -> str:
+    """
+    log_in.
+    """
+    assert True
+    return ""
+
+
+
